516376fc2246248218e2b246f357fa79e0694e15
Zack Wilson <zackazt@gmail.com>
2021-01-01 13:29:22 -0500

    Update route name

--- pages/recognition.js
+++ pages/recognition.js
@@ -1,30 +1,61 @@
+import Head from 'next/head';
 import styles from '../styles/Recognition.module.css';
 import { Toolbar } from '../components/toolbar';
 
-export const Recognition = () => {
+export const Recognition = ({ employee }) => {
   return (
-    <div className="page-container">
-      <Toolbar />
+    <>
+      <Head>
+        <title>Employee Of The Month</title>
+        <meta
+          name="description"
+          content={`This month's employee of the month is ${employee.name}`}
+        />
 
-      <div className={styles.main}>
-        <h1>Employee Of The Month</h1>
+        <meta property="og:image" content={employee.image} />
+        <meta property="og:title" content="Employee Of The Month" />
+        <meta
+          property="og:description"
+          content={`This month's employee of the month is ${employee.name}`}
+        />
 
-        <div></div>
+        <meta property="twitter:image" content={employee.image} />
+        <meta property="twitter:title" content="Employee Of The Month" />
+        <meta
+          property="twitter:description"
+          content={`This month's employee of the month is ${employee.name}`}
+        />
+      </Head>
+
+      <div className="page-container">
+        <Toolbar />
+
+        <div className={styles.main}>
+          <h1>Employee Of The Month</h1>
+
+          <div className={styles.employeeOfTheMonth}>
+            <h3>{employee.name}</h3>
+            <h6>{employee.position}</h6>
+            <img src={employee.image} alt="employee" />
+            <p>{employee.description}</p>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
-// export const getServerSideProps = async pageContext => {
+export const getServerSideProps = async pageContext => {
+  const apiResponse = await fetch(
+    'https://my-json-server.typicode.com/portexe/next-news/employeeOfTheMonth',
+  );
+  const employee = await apiResponse.json();
 
-//   const apiResponse = await fetch('https://my-json-server.typicode.com/portexe/next-news');
-
-//   return {
-//     props: {
-//       articles: articles,
-//       pageNumber: Number.parseInt(pageNumber),
-//     },
-//   };
-// };
+  return {
+    props: {
+      employee,
+    },
+  };
+};
 
 export default Recognition;
